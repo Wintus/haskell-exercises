4e42d378e56058202f83c3d0e94bc018692b14fa
Wintus <132e84e5@opayq.com>
2015-06-26 13:13:48 -0700

    Solve question 2 by implementing qadd, qequal, qlist

--- Src/Chapter3.hs
+++ Src/Chapter3.hs
@@ -34,12 +34,22 @@ module Src.Chapter3
 
     -- (2.1)
     qadd :: (Int, Int) -> (Int, Int) -> (Int, Int)
-    qadd q1 q2 = undefined {- Rewrite HERE! -}
+    qadd (_, 0) (_, _) = error "Zero Division"
+    qadd (_, _) (_, 0) = error "Zero Divison"
+    qadd (a, b) (c, d) = (a * d + b * c, b * d)
 
     -- (2.2)
     qequal :: (Int, Int) -> (Int, Int) -> Bool
-    qequal q1 q2 = undefined {- Rewrite HERE! -}
+    qequal (_, 0) (_, _) = error "Zero Divsion"
+    qequal (_, _) (_, 0) = error "Zero Divsion"
+    qequal (a, b) (c, d) = a * d == b * c
 
     -- (2.3)
     qlist :: (Int, Int) -> [(Int, Int)]
-    qlist q = undefined {- Rewrite HERE! -}
+    qlist (_, 0)         = error "Zero Division"
+    qlist (a, b) | b < 0 = qlist (-a, -b)
+    qlist (a, b)         =
+      let g  = gcd a b
+          a' = a `div` g
+          b' = b `div` g
+      in  [(s * a' * n, s * b' * n) | n <- [1..], s <- [1, -1]]
